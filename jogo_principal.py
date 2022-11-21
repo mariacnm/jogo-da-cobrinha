d67b8b4c6fa5f85e69825b1863eeccf97821b5bf
mariacnm <mariacnm2@al.insper.edu.br>
2022-11-21 16:20:28 -0300

    commit so para limpar

--- jogo_principal.py
+++ jogo_principal.py
@@ -8,7 +8,7 @@ pygame.display.set_caption("Jogo Da Cobrinha")
 
 while True:
     for event in pygame.event.get():
-        if event.type() == QUIT:
+        if event.type= QUIT:
             pygame.quit()
 
     pygame.display.update()
