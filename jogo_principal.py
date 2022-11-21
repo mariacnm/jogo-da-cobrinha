e55a9d7c35c86922876a3e5d77c532652464d1a2
marinnagc <marinnagc@al.insper.edu.br>
2022-11-21 16:11:57 -0300

    iniciando o loop

--- jogo_principal.py
+++ jogo_principal.py
@@ -5,3 +5,10 @@ pygame.init()
 
 window= pygame.display.set_mode((600,600))
 pygame.display.set_caption("Jogo Da Cobrinha")
+
+while True:
+    for event in pygame.event.get():
+        if event.type() == QUIT:
+            pygame.quit()
+
+    pygame.display.update()
