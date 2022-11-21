ca30cdb71cf45fffe9bebda543dddc97902eb8ae
mariacnm <mariacnm2@al.insper.edu.br>
2022-11-21 17:53:24 -0300

    cobrinha andando

--- jogo_principal.py
+++ jogo_principal.py
@@ -7,7 +7,7 @@ pygame.init()
 #posições iniciais
 UP=0
 DOWN=1
-RIGTH=2
+RIGHT=2
 LEFT=3
 
 WIDTH = 600
@@ -23,8 +23,10 @@ cobra_skin= pygame.Surface((10,10))
 cobra_skin.fill((255,255,255))# cor da cobra = branca
 direção= LEFT
 
+clock = pygame.time.Clock()
+
 background = pygame.image.load('grama.png').convert()
-maca_img = pygame.image.load('maça_png-removebg-preview.png').convert_alpha()
+maca_img = pygame.image.load('maça_png-removebg-preview.png').convert_alpha()
 maca_img_small = pygame.transform.scale(maca_img, (MACA_WIDTH, MACA_HEIGHT))
 
 class Maca(pygame.sprite.Sprite):
@@ -41,9 +43,22 @@ class Maca(pygame.sprite.Sprite):
 
 maca = Maca(maca_img_small)
 while True:
+    clock.tick(10)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+    for i in range(len(cobra) - 1, 0, -1):
+        cobra[i] = (cobra[i-1][0], cobra[i-1][1])
+
+    if direção == UP:
+        cobra[0] = (cobra[0][0], cobra[0][1] - 10)
+    if direção == DOWN:
+        cobra[0] = (cobra[0][0], cobra[0][1] + 10)
+    if direção == RIGHT:
+        cobra[0] = (cobra[0][0] + 10, cobra[0][1])
+    if direção == LEFT:
+        cobra[0] = (cobra[0][0] - 10, cobra[0][1])
+
     window.fill((0,0,0)) #para limpar a tela
     for posicao in cobra:
         window.blit(cobra_skin,posicao)
