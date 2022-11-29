2dd2b6d84f0be179bc101a97cbf660f4b7b56c8f
marinnagc <marinnagc@al.insper.edu.br>
2022-11-29 12:52:39 -0300

    erro

--- jogo_principal.py
+++ jogo_principal.py
@@ -12,8 +12,8 @@ LEFT=3
 
 WIDTH = 600
 HEIGHT = 600
-MACA_WIDTH = 20
-MACA_HEIGHT = 20
+MACA_WIDTH = 25
+MACA_HEIGHT = 25
 window= pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Jogo Da Cobrinha")
 
@@ -44,6 +44,7 @@ class Maca(pygame.sprite.Sprite):
 maca = Maca(maca_img_small)
 LPS = 10
 while True:
+    window.blit(background, (0, 0))
     clock.tick(10)
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -75,7 +76,7 @@ while True:
     for posicao in cobra:
         window.blit(cobra_skin,posicao)
 
-    window.blit(background, (0, 0))
+    #window.blit(background, (0, 0))
     window.blit(maca_img_small,maca.rect)
 
     pygame.display.update()
